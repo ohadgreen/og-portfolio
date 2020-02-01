fa070d5c7965a611ff4a80454971e378721c023f
ohadgreen <ohad.green@gmail.com>
2020-02-01 21:55:41 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Link } from "gatsby";
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>I am Ohad, Web developer and owner of two lovely dogs</p>
+            <p>This is my <Link to="/blog">blog</Link></p>
+        </div>
+    )
+}
+
+export default IndexPage;
